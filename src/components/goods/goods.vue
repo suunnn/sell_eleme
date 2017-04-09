95c0604aac9b7381f9f913b6bc07a21c8c15015d
Sun <sunhaowei@zmit.cn>
2017-04-10 02:05:09 +0800

    feat:商品详情menu列表

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,10 +1,63 @@
 <template>
-  <div class="">I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods">
+          <span class="text">
+            <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>
+            {{ item.name }}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script>
-export default {};
+  const ERR_OK = 0;
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: []
+      };
+    },
+    created () {
+      this.getFoods();
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    methods: {
+      getFoods () {
+        this.$http.get('/api/goods')
+          .then((response) => {
+            response = response.body;
+            if (response.errno === ERR_OK) {
+              this.goods = response.data;
+            }
+          });
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .goods
+    display: flex
+    position: absolute
+    width: 100%
+    top: 174px
+    bottom: 46px
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
 </style>
